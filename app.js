17c8fcd62c3793e40637bc4a928b7d20c44ec322
jww52 <jawszone@gmail.com>
2016-11-10 11:16:27 -0700

    first commit think

--- app.js
+++ app.js
@@ -1,1 +1,54 @@
-// your code here!
+//1. On click return analysis: 
+	//A. Total word count of submitted text
+	//B. Unique word count of submitted text
+	//C. Average word length in characters of the submitted text
+	//D. Average sentence length in characters of the submitted text
+
+	$("button").on("click", function(){
+		//total word count
+        var totalWordCount = document.getElementById("user-text").value.toLowerCase().split(" ").length;
+		$(".js-wordCount").text(totalWordCount);
+		
+        //unique word count
+        var wordArray = document.getElementById("user-text").value.toLowerCase().split(" ");
+        var count = 0;
+        var i,
+        	j,
+        	duplicate = false;
+        
+        for (i = 0; i < wordArray.length - 1; i++) {
+       			for (j = i + 1; j < wordArray.length; j++) {
+       			if (i == j) {
+       				duplicate = true;
+       			} else count ++
+       		} 
+       		console.log(count);
+        };
+        $(".js-countUnique").text(count);
+
+        //Average word length in characters of submitted text
+        	function totalWordChar (wrd) {
+        		for (var a = 0; a < wordArray.length; a ++) {
+        			var wrdArray = wordArray[a].toCharArray();
+        			return var wrdArraySum = wrdArray.reduce((a, b) => a + b, 0);
+        		}
+
+        	function avgCharCount () {
+        		return wrdArraySum / (wordArray.length - 1);
+        	}
+        
+       
+        	function myFunction() {
+    			document.getElementById(".js-avgWordLength").innerHTML = wordArray.map(avgCharCount);
+
+        // Average sentence length in characters of the submitted text
+        
+        		
+
+
+
+      $(".hidden").show();
+		
+        event.preventDefault();
+	})
+		
